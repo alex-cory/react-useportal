20517f48d6fcdcb857176172b87644ce39275792
Alex Cory <alexcory@fb.com>
2019-04-10 17:41:28 -0700

    memoizing, new stateful option, alternative ways to attach

--- src/usePortal.js
+++ src/usePortal.js
@@ -1,21 +1,57 @@
-import { useRef, useEffect, useState } from 'react'
-import { findDOMNode, createPortal } from 'react-dom'
+import { useState, useRef, useEffect, useCallback } from 'react'
+import { createPortal, findDOMNode } from 'react-dom'
 
-
-export default function usePortal({
+export const usePortal = ({
   closeOnOutsideClick = true,
   closeOnEsc = true,
+  renderOnClickedElement,
   renderBelowClickedElement, // appear directly under the clicked element/node in the DOM
   bindTo, // attach the portal to this node in the DOM
-  isOpen: defaultIsOpen = false
-} = {}) {
+  isOpen: defaultIsOpen = false,
+  stateful = true
+} = {}) => {
   const [isOpen, setOpen] = useState(defaultIsOpen)
 
+  const renderByRef = useRef()
   const portal = useRef(document.createElement('div'))
   const elToMountTo = (bindTo && findDOMNode(bindTo)) || (document && document.body)
 
+  const handleKeydown = useCallback(e => {
+      var ESC = 27
+      if (!closeOnEsc) return
+      e.keyCode === ESC && stateful && setOpen(false)
+    }, [stateful])
+
+  const openPortal = useCallback(e => {
+    if (e && e.nativeEvent) e.nativeEvent.stopImmediatePropagation()
+    const { left, top, height } = e.target.getBoundingClientRect()
+    if (renderOnClickedElement) {
+      portal.current.style = `
+      height: 0px;
+      position: absolute;
+      left: ${left}px;
+      top: ${top}px;
+    `
+    } else if (renderBelowClickedElement) {
+      portal.current.style = `
+      position: absolute;
+      left: ${left}px;
+      top: ${top + height}px;
+    `
+    }
+    stateful && setOpen(true)
+  }, [stateful, setOpen, portal, renderOnClickedElement])
+
+  const handleOutsideMouseClick = useCallback(
+    ({ target, button }) => {
+      if (portal.current.contains(target) || button !== 0) return
+      stateful && setOpen(false)
+    },
+    [stateful, setOpen]
+  )
+
   useEffect(() => {
-    elToMountTo.appendChild(portal.current);
+    elToMountTo.appendChild(portal.current)
     document.addEventListener('keydown', handleKeydown)
     document.addEventListener('click', handleOutsideMouseClick)
     return () => {
@@ -23,41 +59,19 @@ export default function usePortal({
       document.removeEventListener('click', handleOutsideMouseClick)
       elToMountTo.removeChild(portal.current)
     }
-  }, [elToMountTo])
-
-  var ESC = 27;
-  const handleKeydown = e => closeOnEsc && e.keyCode === ESC && setOpen(false)
-
-  const openPortal = e => {
-    if (e && e.nativeEvent) e.nativeEvent.stopImmediatePropagation()
-    if (renderBelowClickedElement) {
-      const { left, top, height } = e.target.getBoundingClientRect()
-      // TODO: not sure why this isn't working
-      // https://www.w3schools.com/jsref/prop_style_csstext.asp
-      // portal.current.cssText = `
-      //   position: absolute;
-      //   left: ${left}px;
-      //   top: ${top + height}px;
-      // `
-      portal.current.style.position = 'absolute'
-      portal.current.style.left = left + 'px'
-      portal.current.style.top = top + height + 'px'
-    }
-    setOpen(true)
-  }
-
-  const handleOutsideMouseClick = ({ target, button }) => {
-    if (closeOnOutsideClick) {
-      if (portal.current.contains(target) || (button !== 0)) return
-      setOpen(false)
-    }
-  }
+  }, [elToMountTo, handleKeydown, handleOutsideMouseClick])
 
   return {
     isOpen,
     openPortal,
+    onMouseDown: handleKeydown,
+    ref: renderByRef,
     closePortal: () => setOpen(false),
-    togglePortal: e => isOpen ? setOpen(false) : openPortal(e),
-    Portal: ({ children }) => createPortal(children, portal.current)
+    togglePortal: e => (isOpen ? setOpen(false) : openPortal(e)),
+    Portal: ({ children }) => createPortal(children, portal.current),
+    bind: {
+      onMouseDown: handleKeydown,
+      ref: renderByRef
+    }
   }
 }
