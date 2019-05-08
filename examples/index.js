79c33fe3231c8610412dd647bbd52f3aa506decd
Alex Cory <alexcory@fb.com>
2019-05-07 21:48:29 -0700

    bug fix & array destructuring & new example

--- examples/index.js
+++ examples/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import usePortal from '../usePortal'
 
-
-function App() {
+const Exmaple1 = () => {
   const { openPortal, closePortal, isOpen, Portal } = usePortal()
 
   return (
     <>
+      <h3>Example 1</h3>
       <button onClick={openPortal}>Open Portal</button>
       {isOpen && (
         <Portal>
@@ -17,6 +17,56 @@ function App() {
           </div>
         </Portal>
       )}
+    </>
+  )
+}
+
+const Example2 = () => {
+  const {
+    openPortal: openFirstPortal,
+    closePortal: closeFirstPortal,
+    isOpen: isFirstOpen,
+    Portal: FirstPortal
+  } = usePortal();
+
+  const [
+    openSecondPortal,
+    closeSecondPortal,
+    isSecondOpen,
+    SecondPortal
+  ] = usePortal();
+
+  return (
+    <>
+      <h3>Example 2</h3>
+      <button onClick={openFirstPortal}>Open First</button>
+      {isFirstOpen && (
+        <FirstPortal>
+          I'm First.
+          <button
+            onClick={() => {
+              openSecondPortal();
+              closeFirstPortal();
+            }}
+          >
+            Close Me and Open Second
+          </button>
+        </FirstPortal>
+      )}
+      {isSecondOpen && (
+        <SecondPortal>
+          I'm Second<button onClick={closeSecondPortal}>Close Me</button>
+        </SecondPortal>
+      )}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Exmaple1 />
+      <Example2 />
     </>
   )
 }
