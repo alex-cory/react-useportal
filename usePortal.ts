898e5abe2bf2118ebd4b12f80a183a24e0c1b3c5
Alex Cory <alexcory@fb.com>
2019-05-21 23:28:32 -0700

    fixed all SSR issues, added useSSR to fix this, only export ./dist directory to npm

--- usePortal.ts
+++ usePortal.ts
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect, useCallback, useMemo, ReactNode } from 'react'
 import { createPortal, findDOMNode } from 'react-dom'
+import useSSR from 'use-ssr'
 
 interface UsePortalOptions {
   closeOnOutsideClick?: boolean,
@@ -11,9 +12,8 @@ interface UsePortalOptions {
   stateful?: boolean,
 }
 
-const emptyFunction = () => {}
 
-export function usePortal({
+export default function usePortal({
   closeOnOutsideClick = true,
   closeOnEsc = true,
   renderOnClickedElement,
@@ -22,31 +22,11 @@ export function usePortal({
   isOpen: defaultIsOpen = false,
   stateful = true,
 }: UsePortalOptions = {}) {
-  const isBrowser = useRef(false)
-  useEffect(() => {
-    isBrowser.current = true
-    return () => {
-      isBrowser.current = false
-    }
-  }, [])
-
-  // if we're not on the browser, don't continue
-  if (!isBrowser.current) return Object.assign([emptyFunction, emptyFunction, false, emptyFunction], {
-    isOpen: false,
-    openPortal: emptyFunction,
-    onMouseDown: emptyFunction,
-    ref: null,
-    closePortal: emptyFunction,
-    togglePortal: emptyFunction,
-    Portal: emptyFunction,
-    bind: {
-      onMouseDown: emptyFunction,
-      ref: null
-    }
-  })
-
+  const { isServer, isBrowser } = useSSR()
   const [isOpen, makeOpen] = useState(defaultIsOpen)
+  // we use this ref because `isOpen` is stale for handleOutsideMouseClick
   const open = useRef(isOpen)
+
   const setOpen = useCallback(v => {
     // workaround to not have stale `isOpen` in the handleOutsideMouseClick
     open.current = v
@@ -54,56 +34,78 @@ export function usePortal({
   }, [isOpen, open.current])
 
   const renderByRef = useRef()
-  const portal = useRef(document && document.createElement('div'))
-  const elToMountTo = useMemo(() => (bindTo && findDOMNode(bindTo)) || (document && document.body), [])
+  const portal = useRef(isBrowser && document.createElement('div'))
+
+  useEffect(() => {
+    if (isBrowser && !portal.current) portal.current = document.createElement('div')
+  }, [isBrowser])
+
+  const elToMountTo = useMemo(() => {
+    if (isServer) return
+    return ((bindTo && findDOMNode(bindTo)) || document.body)
+  }, [isServer, bindTo])
 
   const handleKeydown = useCallback(e => {
     var ESC = 27
     if (e.keyCode === ESC && stateful && closeOnEsc) setOpen(false)
-  }, [])
+  }, [closeOnEsc, stateful, setOpen])
 
   const openPortal = useCallback(e => {
+    if (isServer) return
     // for some reason, when we don't have the event argument there
     // is a weird race condition, would like to see if we can remove
     // setTimeout, but for now this works
     if (e == null) return setTimeout(() => stateful && setOpen(true), 0)
     if (e && e.nativeEvent) e.nativeEvent.stopImmediatePropagation()
     const { left, top, height } = e.target.getBoundingClientRect()
-    if (renderOnClickedElement) {
+    if (renderOnClickedElement && portal.current instanceof HTMLElement) {
       portal.current.style.height = '0px';
       portal.current.style.position = 'absolute';
       portal.current.style.left = left + 'px';
       portal.current.style.top = top + 'px';
-    } else if (renderBelowClickedElement) {
+    } else if (renderBelowClickedElement && portal.current instanceof HTMLElement) {
       portal.current.style.position = 'absolute';
       portal.current.style.left = left + 'px';
       portal.current.style.top = top + height + 'px';
     }
     stateful && setOpen(true)
-  }, [stateful, portal])
+  }, [isServer, stateful, portal, setOpen, renderBelowClickedElement, renderOnClickedElement])
 
   const closePortal = useCallback(() => {
+    if (isServer) return
     if (open.current) setOpen(false)
-  }, [isOpen, open.current])
+  }, [isServer, isOpen, open.current, setOpen])
+
+  const togglePortal = useCallback(e => (
+    isOpen ? setOpen(false) : openPortal(e)
+  ), [isOpen, open.current, setOpen, openPortal])
 
   const handleOutsideMouseClick = useCallback(({ target, button }) => {
-    if (portal.current.contains(target) || button !== 0 || !portal.current || !open.current) return
+    if (isServer || !(portal.current instanceof HTMLElement)) return
+    if (portal.current.contains(target) || button !== 0 || !open.current) return
     if (stateful && closeOnOutsideClick) closePortal()
-  }, [isOpen])
+  }, [isServer, isOpen, stateful, closePortal, closeOnOutsideClick])
 
   useEffect(() => {
+    if (isServer || !(elToMountTo instanceof HTMLElement) || !(portal.current instanceof HTMLElement)) return
+
+    const node = portal.current
     elToMountTo.appendChild(portal.current)
     document && document.addEventListener('keydown', handleKeydown)
     document && document.addEventListener('click', handleOutsideMouseClick)
+
     return () => {
       document && document.removeEventListener('keydown', handleKeydown)
       document && document.removeEventListener('click', handleOutsideMouseClick)
-      elToMountTo.removeChild(portal.current)
+      elToMountTo.removeChild(node)
     }
-  }, [handleOutsideMouseClick, handleKeydown])
 
-  const togglePortal = useCallback(e => isOpen ? setOpen(false) : openPortal(e), [isOpen, open.current])
-  const Portal = ({ children }: { children: ReactNode }) => createPortal(children, portal.current)
+  }, [isServer, handleOutsideMouseClick, handleKeydown, elToMountTo])
+
+  const Portal = ({ children }: { children: ReactNode }) => {
+    if (portal.current instanceof HTMLElement) return createPortal(children, portal.current)
+    return null
+  }
 
   return Object.assign([
       openPortal,
@@ -127,5 +129,3 @@ export function usePortal({
     }
   )
 }
-
-export default usePortal
