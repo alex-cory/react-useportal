d3e0b17f7fd7e76e9e130e2946b0407ef77d0332
Alex Cory <results@alexcory.com>
2019-09-13 10:21:43 -0700

    dependency fixes

--- usePortal.ts
+++ usePortal.ts
@@ -3,7 +3,11 @@ import { createPortal, findDOMNode } from 'react-dom'
 import useSSR from 'use-ssr'
 
 type HTMLElRef = MutableRefObject<HTMLElement>
-type CustomEvent = { event?: SyntheticEvent<any, Event>, portal: HTMLElRef, targetEl: HTMLElement }
+type CustomEvent = {
+  event?: SyntheticEvent<any, Event>
+  portal: HTMLElRef
+  targetEl: HTMLElement
+}
 type CustomEventHandler = (customEvent: CustomEvent) => void | HTMLElRef
 type EventHandlers = {
   [K in keyof DOMAttributes<K>]?: EventHandler<SyntheticEvent<any, Event>>
@@ -24,6 +28,8 @@ type UsePortalOptions = {
 type UsePortalObjectReturn = {} // TODO
 type UsePortalArrayReturn = [] // TODO
 
+const errorMessage1 = 'You must either bind to an element or pass an event to openPortal(e).'
+
 export default function usePortal({
   closeOnOutsideClick = true,
   closeOnEsc = true,
@@ -40,28 +46,30 @@ export default function usePortal({
   // we use this ref because `isOpen` is stale for handleOutsideMouseClick
   const open = useRef(isOpen)
 
-  const setOpen = useCallback(v => {
+  const setOpen = useCallback((v: boolean) => {
     // workaround to not have stale `isOpen` in the handleOutsideMouseClick
     open.current = v
     makeOpen(v)
-  }, [isOpen, open.current])
+  }, [])
 
   const targetEl = useRef() as HTMLElRef // this is the element you are clicking/hovering/whatever, to trigger opening the portal
   const portal = useRef(isBrowser ? document.createElement('div') : null) as HTMLElRef
 
   useEffect(() => {
     if (isBrowser && !portal.current) portal.current = document.createElement('div')
-  }, [isBrowser])
+  }, [isBrowser, portal])
 
   const elToMountTo = useMemo(() => {
     if (isServer) return
-    return ((bindTo && findDOMNode(bindTo)) || document.body)
+    return (bindTo && findDOMNode(bindTo)) || document.body
   }, [isServer, bindTo])
 
-  const handleKeydown = useCallback(e => {
-    var ESC = 27
-    if (e.keyCode === ESC && closeOnEsc) closePortal()
-  }, [closeOnEsc, setOpen])
+  const handleEvent = useCallback((func: CustomEventHandler, event?: SyntheticEvent<any, Event>) => {
+    if (event && event.currentTarget) targetEl.current = event.currentTarget as HTMLElement
+    // i.e. onClick, etc. inside usePortal({ onClick({ portal, targetEl }) {} })
+    const maybePortal = func({ portal, targetEl: targetEl.current as HTMLElement, event })
+    if (maybePortal) portal.current = maybePortal.current
+  }, [portal, targetEl]) 
 
   const openPortal = useCallback((event: SyntheticEvent<any, Event>) => {
     if (isServer) return
@@ -70,11 +78,11 @@ export default function usePortal({
     // setTimeout, but for now this works
     if (event == null && targetEl.current == null) {
       setTimeout(() => setOpen(true), 0)
-      throw Error('You must either bind to an element or pass an event to openPortal(e).')
+      throw Error(errorMessage1)
     }
     if (event && event.nativeEvent) event.nativeEvent.stopImmediatePropagation()
     if (event) targetEl.current = event.currentTarget
-    if (!targetEl.current) throw Error('You must either bind to an element or pass an event to openPortal(e).')
+    if (!targetEl.current) throw Error(errorMessage1)
     const { left, top } = targetEl.current.getBoundingClientRect()
     if (onOpen) handleEvent(onOpen, event)
     if (renderOnClickedElement && portal.current instanceof HTMLElement) {
@@ -92,31 +100,30 @@ export default function usePortal({
       `
     }
     setOpen(true)
-  }, [isServer, portal, setOpen, renderBelowClickedElement, renderOnClickedElement])
-
-  const handleEvent = (func: CustomEventHandler, event?: SyntheticEvent<any, Event>) => {
-    if (event && event.currentTarget) targetEl.current = event.currentTarget as HTMLElement
-    // i.e. onClick, etc. inside usePortal({ onClick({ portal, targetEl }) {} })
-    const maybePortal = func({ portal, targetEl: targetEl.current as HTMLElement, event })
-    if (maybePortal) portal.current = maybePortal.current
-  }
+  }, [isServer, portal, setOpen, renderBelowClickedElement, renderOnClickedElement, handleEvent, targetEl, onOpen])
 
   const closePortal = useCallback((event?: SyntheticEvent<any, Event>) => {
     if (isServer) return
     if (onClose) handleEvent(onClose, event)
     if (open.current) setOpen(false)
-  }, [isServer, isOpen, open.current, setOpen])
+  }, [isServer, handleEvent, onClose, setOpen])
 
-  const togglePortal = useCallback((e: SyntheticEvent<any, Event>) => (
-    isOpen ? closePortal(e) : openPortal(e)
-  ), [isOpen, open.current, closePortal, openPortal])
+  const togglePortal = useCallback((e: SyntheticEvent<any, Event>) =>
+    isOpen ? closePortal(e) : openPortal(e),
+    [isOpen, closePortal, openPortal]
+  )
 
-  const handleOutsideMouseClick = useCallback(e => {
+  const handleKeydown = useCallback(e => {
+    var ESC = 27
+    if (e.keyCode === ESC && closeOnEsc) closePortal(e)
+  }, [closeOnEsc, closePortal])
+
+  const handleOutsideMouseClick = useCallback(e  => {
     if (isServer) return
     if (!(portal.current instanceof HTMLElement)) return
     if (portal.current.contains(e.target) || e.button !== 0 || !open.current) return
     if (closeOnOutsideClick) closePortal(e)
-  }, [isServer, isOpen, closePortal, closeOnOutsideClick])
+  }, [isServer, closePortal, closeOnOutsideClick, portal])
 
   useEffect(() => {
     if (isServer) return
@@ -132,13 +139,12 @@ export default function usePortal({
       document && document.removeEventListener('click', handleOutsideMouseClick)
       elToMountTo.removeChild(node)
     }
-
-  }, [isServer, handleOutsideMouseClick, handleKeydown, elToMountTo])
+  }, [isServer, handleOutsideMouseClick, handleKeydown, elToMountTo, portal])
 
   const Portal = useCallback(({ children }: { children: ReactNode }) => {
     if (portal.current != null) return createPortal(children, portal.current)
     return null
-  }, [portal.current])
+  }, [portal])
 
   // this should handle all eventHandlers like onClick, onMouseOver, etc. passed into the config
   const customEventHandlers: EventHandlers = Object
@@ -146,17 +152,10 @@ export default function usePortal({
     .reduce<any>((acc, [handlerName, eventHandler]) => {
       acc[handlerName] = (event?: SyntheticEvent<any, Event>) => handleEvent(eventHandler, event)
       return acc
-    },
-    {}
-  )
+    }, {})
 
-  return Object.assign([
-      openPortal,
-      closePortal,
-      open.current,
-      Portal,
-      togglePortal
-    ],
+  return Object.assign(
+    [openPortal, closePortal, open.current, Portal, togglePortal],
     {
       isOpen: open.current,
       openPortal,
@@ -169,7 +168,7 @@ export default function usePortal({
       bind: {
         onMouseDown: handleKeydown,
         ref: targetEl,
-        ...customEventHandlers,
+        ...customEventHandlers
       }
     }
   )
